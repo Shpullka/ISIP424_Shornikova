8e6c224adbedea28e2596ca03a2c6af13e0d4a2d
243543 <243543@KB37-118-C11>
2026-09-09 14:22:16 +0300

    Добавьте файлы проекта.

---
+++ ISIP424_Shornikova/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISIP424_Shornikova
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
