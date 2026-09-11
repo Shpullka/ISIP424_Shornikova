3410d67510fa4b45c41996d117bacbc474724256
Shpullka <shora.y@mail.ru>
2026-09-11 15:30:02 +0300

    Сделаны перечисления для категорий товаров; создан класс и параметры товаров; проверка количества товаров и их вывод на экран с информацией; создан лист для товаров; и счетчик, начинающий счет с 1

--- ISIP424_Shornikova/Program.cs
+++ ISIP424_Shornikova/Program.cs
@@ -8,8 +8,38 @@ namespace ISIP424_Shornikova
 {
     internal class Program
     {
+        public enum ProductCategory
+        {
+            Электроника,
+            Одежда,
+            Бытовая_техника,
+            Продукты
+        }
+
+        public class Product
+        {
+            public int ProductID { get; set; }
+            public string Name { get; set; }
+            public decimal Price { get; set; }
+            public int Quantity { get; set; }
+            public ProductCategory Category { get; set; }
+
+            public bool InStock => Quantity > 0;
+            public override string ToString()
+            {
+                return $"Код: {ProductID}\n" + $"Название: {Name}\n" + $"Цена: {Price}\n" + $"Количество: {Quantity}\n" + $"Категория: {Category}\n" + $"В наличии: {(InStock ? "Да" : "Нет")}\n";
+            }
+        }
+
+        class List
+        {
+            static List<Product> Products = new List<Product>();
+            static int Code = 1001;
+        }
+
         static void Main(string[] args)
         {
+            
         }
     }
 }
